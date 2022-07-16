54d3b1af1aad6cda0e984d06362d1b0ae94ad9e9
eyzi <contact@eyzi.dev>
2022-07-16 19:43:25 -0400

    feat(projects): hide until logo is clicked

--- src/components/MoonlitLogo.vue
+++ src/components/MoonlitLogo.vue
@@ -1,15 +1,28 @@
 <template>
   <q-img
+    id="logo"
     class="q-my-xl"
     width="250px"
     src="https://cdn.moonlit.works/www/moonlit-logo.png"
+    @click.prevent="showProjects"
   ></q-img>
 </template>
 
 <script setup lang="ts">
+import events from './events';
 import { defineComponent } from 'vue';
+
+function showProjects() {
+  events.emit('logo:click')
+}
 
 defineComponent({
   name: 'MoonlitLogo',
 })
 </script>
+
+<style lang="scss" scoped>
+#logo {
+  cursor: pointer;
+}
+</style>
--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -1,16 +1,18 @@
 <template>
-  <div v-if="projectList.length > 0" id="projects-section" class="row items-center justify-center">
+  <div v-if="showProjects && projectList.length > 0" id="projects-section" class="row items-center justify-center">
     <ProjectItem v-for="project in projectList" :key="project.id" :project="project" />
   </div>
 </template>
 
 <script setup lang="ts">
+import events from './events';
 import { defineComponent, onMounted, ref } from 'vue';
 import { api } from 'src/boot/axios';
 import { ProjectType } from './models';
 
 import ProjectItem from './ProjectItem.vue';
 
+let showProjects = ref(false);
 let projectList = ref<ProjectType[]>([])
 
 onMounted(() => {
@@ -21,8 +23,13 @@ onMounted(() => {
       .map(mapProjectList))
     .then(list => {
       projectList.value = list;
+      events.on('logo:click', toggleShowProjects);
     });
 });
+
+function toggleShowProjects() {
+  showProjects.value = !showProjects.value;
+}
 
 function sortByReleaseDate(a: ProjectType, b: ProjectType) {
   return new Date(b.releaseDate ?? -Infinity).getTime() - new Date(a.releaseDate ?? -Infinity).getTime()
@@ -53,6 +60,33 @@ defineComponent({
 #projects-section {
   margin: 50px 0px;
   max-width: 80vw;
+}
+
+#minimized-projects-section {
+  position: relative;
+  cursor: pointer;
+  margin: 10px;
+  opacity: 0.5;
+  font-size: 1.2em;
+  letter-spacing: 0.3em;
+  transition: opacity .2s;
+}
+
+#minimized-projects-section:hover {
+  opacity: 1;
+}
+
+
+.upsidedown {
+  transform:rotate(180deg); 
+  -webkit-transform:rotate(180deg);
+  -o-transform:rotate(180deg);
+  -ms-transform:rotate(180deg);
+}
+.upsidedown.caret {
+  display: inline-block; 
+  position:relative; 
+  bottom: 0.15em;
 }
 
 @media only screen and (max-width: 600px) {
